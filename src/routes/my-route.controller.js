c4f3d328c03a208e1190c584a4f0b1551cc1edc6
OlenaYashyna <lena.yashyna@gmail.com>
2018-07-02 10:44:06 +0300

    ng-show

--- src/routes/my-route.controller.js
+++ src/routes/my-route.controller.js
@@ -4,10 +4,16 @@ export default class MyRouteController {
             login : 'Your Name',
             password: 'Your Password'
         }
+        $scope.showForm = true;
+        $scope.showStatus = false;
         //$scope.handleSubmit = () => console.log(this);
         $scope.handleSubmit = function() { 
             const userData = JSON.stringify(this.formData);
             localStorage.setItem("myKey", userData);
+            $scope.showForm = false;
+            $scope.showStatus = true;
+
+
             
             
             console.log(this);
