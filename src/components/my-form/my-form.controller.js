1a495efd0ab02a7072ec9e527c7e53b39043733f
OlenaYashyna <lena.yashyna@gmail.com>
2018-07-03 19:06:57 +0300

    auth

--- src/components/my-form/my-form.controller.js
+++ src/components/my-form/my-form.controller.js
@@ -2,5 +2,5 @@ export default function ($scope) {
 
      //console.log (this, $scope, $scope.$ctrl.formData)
     
-    this.onSubmit = () => this.handleSubmit({ login: this.login, password: this.password });
+    this.onSubmit = () => this.handleSubmit({ login: this.login, password: this.password, $location: this.$location });
 }
